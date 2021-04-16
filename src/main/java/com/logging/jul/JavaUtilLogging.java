e696c5672a25ead8c0aa99ce26f2d7e0f49d8e65
Paul Samsotha <paulsamsotha.dev@gmail.com>
2021-04-16 16:24:14 -0700

    Add Gradle Wrapper

---
+++ src/main/java/com/logging/jul/JavaUtilLogging.java
@@ -0,0 +1,16 @@
+package com.logging.jul;
+
+import java.util.logging.Logger;
+
+public class JavaUtilLogging {
+
+    private static final Logger LOGGER = Logger.getLogger(JavaUtilLogging.class.getName());
+
+    private static void main(String... args) {
+
+    }
+
+    private static void doLoggingTask() {
+
+    }
+}
